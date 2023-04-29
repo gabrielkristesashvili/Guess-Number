360fe09cc7fdafd8b51fb88f90dabe5335660a06
gabrielkristesashvili <qristesashvili0@gmail.com>
2023-04-30 00:06:59 +0400

    bodyColor

--- script.js
+++ script.js
@@ -20,10 +20,19 @@ document.querySelector('.number').textContent = secretNumber
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value)
     console.log(guess, typeof guess);
+
+    // ? when there is no input
     if (!guess) {
         document.querySelector('.message').textContent = "⛔ No number!"
+
+        //? When player wins
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = "🎉 Correct Number!"
+        document.querySelector('body').style.backgroundColor = '#60b347'
+
+        document.querySelector('.number').style.width = '30rem'
+
+        //? when guess is too high
     } else if (guess > secretNumber) {
         if (score > 1) {
             score--
@@ -33,6 +42,8 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.message').textContent = "💥 You lost the game!"
             document.querySelector('.score').textContent = 0
         }
+
+        //? when guess is too low
     } else if (guess < secretNumber) {
         if (score > 1) {
             score--
