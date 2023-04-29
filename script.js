ac7dbdf1968deabdce0bd85ff911d61782cf9410
gabrielkristesashvili <qristesashvili0@gmail.com>
2023-04-30 00:20:29 +0400

    finish game principes

--- script.js
+++ script.js
@@ -12,10 +12,9 @@
 // document.querySelector('.guess').value = 23
 // console.log(document.querySelector('.guess').value)
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1
+let secretNumber = Math.trunc(Math.random() * 20) + 1
 let score = 20
 
-document.querySelector('.number').textContent = secretNumber
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value)
@@ -29,8 +28,11 @@ document.querySelector('.check').addEventListener('click', function () {
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = "🎉 Correct Number!"
         document.querySelector('body').style.backgroundColor = '#60b347'
+        document.querySelector('.number').textContent = secretNumber
 
         document.querySelector('.number').style.width = '30rem'
+        document.querySelector('.guess').setAttribute('disabled', 'disabled')
+
 
         //? when guess is too high
     } else if (guess > secretNumber) {
@@ -54,5 +56,21 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.score').textContent = 0
         }
     }
+
+})
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20
+    secretNumber = Math.trunc(Math.random() * 20) + 1
+
+    document.querySelector('.message').textContent = "Start guessing..."
+    document.querySelector('.score').textContent = score
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.guess').value = ''
+
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.number').style.width = '15rem'
+    document.querySelector('.guess').removeAttribute('disabled')
+
 
 })
