33c4dabf3594de3b848610f529de6b1c6705cb6a
gabrielkristesashvili <qristesashvili0@gmail.com>
2023-04-30 02:15:58 +0400

    transfer html code to javascript for fun

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 'use strict';
 
-
+document.querySelector('body').innerHTML = `
+    <header>
+        <h1>Guess My Number!</h1>
+        <p class="between">(Between 1 and 20)</p>
+        <button class="btn again">Again!</button>
+        <div class="number">?</div>
+    </header>
+    <main>
+        <section class="left">
+        <input type="number" class="guess" />
+        <button class="btn check">Check!</button>
+    </section>
+    <section class="right">
+        <p class="message">Start guessing...</p>
+        <p class="label-score">💯 Score: <span class="score">20</span></p>
+        <p class="label-highscore">
+        🥇 Highscore: <span class="highscore">0</span>
+        </p>
+    </section>
+    </main>
+    `
 //* dom manipulation
 
 // console.log(document.querySelector('.message').textContent);
@@ -22,7 +42,6 @@ const displayMessage = function (message) {
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value)
-    console.log(guess, typeof guess);
 
     // ? when there is no input
     if (!guess) {
