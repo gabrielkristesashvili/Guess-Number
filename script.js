c32ea01cccf105c317f42cc33c7de1a832697552
gabrielkristesashvili <qristesashvili0@gmail.com>
2023-04-29 23:36:44 +0400

    smthnew

--- script.js
+++ script.js
@@ -3,11 +3,24 @@
 
 //* dom manipulation
 
-console.log(document.querySelector('.message').textContent);
-console.log(document.querySelector('.message').textContent = "Correct Number 😍");
+// console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent = "Correct Number 😍");
 
-document.querySelector('.number').textContent = 13
-document.querySelector('.score').textContent = 10
+// document.querySelector('.number').textContent = 13
+// document.querySelector('.score').textContent = 10
 
-document.querySelector('.guess').value = 23
-console.log(document.querySelector('.guess').value)
+// document.querySelector('.guess').value = 23
+// console.log(document.querySelector('.guess').value)
+
+const x = function () {
+    console.log(23);
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value)
+    console.log(guess, typeof guess);
+    if (!guess) {
+        document.querySelector('.message').textContent = "⛔ No number !"
+    }
+
+})
